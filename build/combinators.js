a07b55adc1a7f6d7ccbc4da3752fa805674add37
Igor Duarte <igor.gsduarte@gmail.com>
2022-02-17 18:36:35 -0300

    Rebuild

--- build/combinators.js
+++ build/combinators.js
@@ -29,7 +29,8 @@ var Types = _interopRequireWildcard(_types);
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 const runProcess = Process.runProcess;
-const Query = _Query.Query;
+
+const Query = _Query.default;
 const getIterator = Types.getIterator,
       hasIterator = Types.hasIterator;
 function delay(seconds) {
